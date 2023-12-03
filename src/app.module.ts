2e9b215f383c5b6b1c97def5a0c83904230aba5a
choewy <choewy32@gmail.com>
2023-12-03 13:47:49 +0900

    feature/inject modules into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BeforeApplicationShutdown, Module, OnApplicationBootstrap } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+
+import { RedisConfig } from '@/common';
+import { RedisClient } from '@/core';
+import { RedisClientModule, RedisLuascriptModule } from '@/module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), RedisClientModule, RedisLuascriptModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap, BeforeApplicationShutdown {
+  private async flushdb() {
+    const redis = new RedisClient(new RedisConfig().getOptions());
+    await redis.connect();
+    await redis.flushdb();
+
+    redis.disconnect();
+  }
+
+  async onApplicationBootstrap() {
+    await this.flushdb();
+  }
+
+  async beforeApplicationShutdown() {
+    await this.flushdb();
+  }
+}
