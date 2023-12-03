99c68c3f7f891a17b64c1fc8b12309ad859fee38
choewy <choewy32@gmail.com>
2023-12-03 13:14:19 +0900

    feature/replace redis with ioredis

--- src/common/configs/redis.config.ts
+++ src/common/configs/redis.config.ts
@@ -1,21 +1,24 @@
-import { RedisClientOptions } from 'redis';
+import { RedisOptions } from 'ioredis';
 
 import { ConfigService } from '@nestjs/config';
 
 export class RedisConfig {
   private readonly configService = new ConfigService();
 
-  private readonly URL = this.configService.get<string>('REDIS_URL');
+  private readonly HOST = this.configService.get<string>('REDIS_HOST');
+  private readonly PORT = this.configService.get<string>('REDIS_PORT');
   private readonly USERNAME = this.configService.get<string>('REDIS_USERNAME');
   private readonly PASSWORD = this.configService.get<string>('REDIS_PASSWORD');
   private readonly DATABASE = this.configService.get<string>('REDIS_DATABASE');
 
-  getClientOptions(): RedisClientOptions {
+  getOptions(): RedisOptions {
     return {
-      url: this.URL,
+      host: this.HOST,
+      port: Number(this.PORT ?? 6375),
       username: this.USERNAME,
       password: this.PASSWORD,
-      database: Number(this.DATABASE ?? 0),
+      db: Number(this.DATABASE ?? 0),
+      lazyConnect: true,
     };
   }
 }
